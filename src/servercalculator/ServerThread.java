30d7ed6455c50b3bc5a9a74918c93875f40661e5
david <david@DESKTOP-0H86BAP>
2016-03-22 17:07:28 +0100

    menjan Thread zbog dodavanja dugmica +/-

--- src/servercalculator/ServerThread.java
+++ src/servercalculator/ServerThread.java
@@ -15,6 +15,51 @@ public class ServerThread extends Thread {
     public ServerThread(Socket clientSocket) {
         this.clientSocket = clientSocket;
     }
+    //String regex = [0-9]
+
+    public static double sabiranje(String znak, String odKlijenta) {
+        String[] brojeviString = odKlijenta.split(znak);
+        double[] brojevi = new double[brojeviString.length];
+        brojevi[0] = Double.parseDouble(brojeviString[0]);
+        brojevi[1] = Double.parseDouble(brojeviString[1]);
+
+        double zbir = brojevi[0] + brojevi[1];
+        return zbir;
+
+    }
+
+    public double oduzimanje(String znak, String odKlijenta) {
+        String[] brojeviString = odKlijenta.split(znak);
+        double[] brojevi = new double[brojeviString.length];
+        brojevi[0] = Double.parseDouble(brojeviString[0]);
+        brojevi[1] = Double.parseDouble(brojeviString[1]);
+
+        double razlika = brojevi[0] - brojevi[1];
+        return razlika;
+
+    }
+
+    public double mnozenje(String znak, String odKlijenta) {
+        String[] brojeviString = odKlijenta.split(znak);
+        double[] brojevi = new double[brojeviString.length];
+        brojevi[0] = Double.parseDouble(brojeviString[0]);
+        brojevi[1] = Double.parseDouble(brojeviString[1]);
+
+        double proizvod = brojevi[0] * brojevi[1];
+        return proizvod;
+
+    }
+
+    public double deljenje(String znak, String odKlijenta) {
+        String[] brojeviString = odKlijenta.split(znak);
+        double[] brojevi = new double[brojeviString.length];
+        brojevi[0] = Double.parseDouble(brojeviString[0]);
+        brojevi[1] = Double.parseDouble(brojeviString[1]);
+
+        double kolicnik = brojevi[0] / brojevi[1];
+        return kolicnik;
+
+    }
 
     @Override
     public void run() {
@@ -24,50 +69,78 @@ public class ServerThread extends Thread {
 
             String ulazOdKalkulatora = inputStreamClient.readLine();
 
-            if (ulazOdKalkulatora.contains("+")) {
-                String[] brojeviString = ulazOdKalkulatora.split("\\+");
-                double[] brojevi = new double[brojeviString.length];
-                brojevi[0] = Double.parseDouble(brojeviString[0]);
-                brojevi[1] = Double.parseDouble(brojeviString[1]);
+            if (ulazOdKalkulatora.startsWith("-")) {
+                String ulazOdKalkulatoraBezZnaka = ulazOdKalkulatora.substring(1);
+                if (ulazOdKalkulatoraBezZnaka.contains("+")) {
+                    String[] brojeviString = ulazOdKalkulatoraBezZnaka.split("\\+");
+                    double[] brojevi = new double[brojeviString.length];
+                    brojevi[0] = Double.parseDouble(brojeviString[0]);
+                    brojevi[1] = Double.parseDouble(brojeviString[1]);
+                    double zbir = brojevi[1] - brojevi[0];
+                    String rezultatZbir = "" + zbir;
+                    outputStreamClient.println(rezultatZbir);
 
-                double zbir = brojevi[0] + brojevi[1];
-                String rezultatZbir = "" + zbir;
-                outputStreamClient.println(rezultatZbir);
+                }
+                if (ulazOdKalkulatoraBezZnaka.contains("-")) {
+                    String[] brojeviString = ulazOdKalkulatoraBezZnaka.split("-");
+                    double[] brojevi = new double[brojeviString.length];
+                    brojevi[0] = Double.parseDouble(brojeviString[0]);
+                    brojevi[1] = Double.parseDouble(brojeviString[1]);
+                    double razlika = -brojevi[0] - brojevi[1];
 
+                    String rezultatRazlika = "" + razlika;
+                    outputStreamClient.println(rezultatRazlika);
+
+                }
+                if (ulazOdKalkulatoraBezZnaka.contains("*")) {
+                    String[] brojeviString = ulazOdKalkulatoraBezZnaka.split("\\*");
+                    double[] brojevi = new double[brojeviString.length];
+                    brojevi[0] = Double.parseDouble(brojeviString[0]);
+                    brojevi[1] = Double.parseDouble(brojeviString[1]);
+                    double proizvod = -(brojevi[0] * brojevi[1]);
+
+                    String rezultatMnozenje = "" + proizvod;
+                    outputStreamClient.println(rezultatMnozenje);
+
+                }
+                if (ulazOdKalkulatoraBezZnaka.contains("/")) {
+                    String[] brojeviString = ulazOdKalkulatoraBezZnaka.split("/");
+                    double[] brojevi = new double[brojeviString.length];
+                    brojevi[0] = Double.parseDouble(brojeviString[0]);
+                    brojevi[1] = Double.parseDouble(brojeviString[1]);
+                    double kolicnik = -(brojevi[1] / brojevi[0]);
+
+                    String rezultatDeljenje = "" + kolicnik;
+                    outputStreamClient.println(rezultatDeljenje);
+
+                }
+
+            } else {
+                if (ulazOdKalkulatora.contains("+")) {
+                    String rezultatZbir = "" + sabiranje("\\+", ulazOdKalkulatora);
+                    outputStreamClient.println(rezultatZbir);
+
+                }
+                if (ulazOdKalkulatora.contains("-")) {
+
+                    String rezultatRazlika = "" + oduzimanje("-", ulazOdKalkulatora);
+                    outputStreamClient.println(rezultatRazlika);
+
+                }
+                if (ulazOdKalkulatora.contains("*")) {
+
+                    String rezultatMnozenje = "" + mnozenje("\\*", ulazOdKalkulatora);
+                    outputStreamClient.println(rezultatMnozenje);
+
+                }
+                if (ulazOdKalkulatora.contains("/")) {
+
+                    String rezultatDeljenje = "" + deljenje("/", ulazOdKalkulatora);
+                    outputStreamClient.println(rezultatDeljenje);
+
+                }
             }
-            if (ulazOdKalkulatora.contains("-")) {
-                String[] brojeviString = ulazOdKalkulatora.split("-");
-                double[] brojevi = new double[brojeviString.length];
-                brojevi[0] = Double.parseDouble(brojeviString[0]);
-                brojevi[1] = Double.parseDouble(brojeviString[1]);
 
-                double razlika = brojevi[0] - brojevi[1];
-                String rezultatRazlika = "" + razlika;
-                outputStreamClient.println(rezultatRazlika);
-
-            }
-            if (ulazOdKalkulatora.contains("*")) {
-                String[] brojeviString = ulazOdKalkulatora.split("\\*");
-                double[] brojevi = new double[brojeviString.length];
-                brojevi[0] = Double.parseDouble(brojeviString[0]);
-                brojevi[1] = Double.parseDouble(brojeviString[1]);
-
-                double mnozenje = brojevi[0] * brojevi[1];
-                String rezultatMnozenje = "" + mnozenje;
-                outputStreamClient.println(rezultatMnozenje);
-
-            }
-            if (ulazOdKalkulatora.contains("/")) {
-                String[] brojeviString = ulazOdKalkulatora.split("/");
-                double[] brojevi = new double[brojeviString.length];
-                brojevi[0] = Double.parseDouble(brojeviString[0]);
-                brojevi[1] = Double.parseDouble(brojeviString[1]);
-
-                double deljenje = brojevi[0] / brojevi[1];
-                String rezultatDeljenje = "" + deljenje;
-                outputStreamClient.println(rezultatDeljenje);
-
-            }
             clientSocket.close();
         } catch (IOException ex) {
             Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
